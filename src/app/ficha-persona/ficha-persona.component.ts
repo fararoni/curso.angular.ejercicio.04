9c3a37a786c1facc71162f915243af486833ee2d
Isaí Fararoni <fararoni@gmail.com>
2024-05-26 23:02:08 -0600

    Add files via upload

---
+++ src/app/ficha-persona/ficha-persona.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ficha-persona',
+  standalone: true,
+  imports: [],
+  templateUrl: './ficha-persona.component.html',
+  styleUrl: './ficha-persona.component.css'
+})
+export class FichaPersonaComponent {
+
+}
